1f773899a76e2b5af40dd8815651e32d265ad0fb
Mr-Freeman-777 <82430178+Mr-Freeman-777@users.noreply.github.com>
2021-04-30 07:47:54 +0300

    Add files via upload

---
+++ PracticZad_1.py
@@ -0,0 +1,61 @@
+#Задание №1
+
+#duration = input('duration =')
+#print(duration, 'сек')
+
+#duration = input('duration =')
+#a = float(duration)
+#min = a // 60
+#sek = a % 60
+#min_int = int(min)
+#sek_int = int(sec)
+#print( min_int , 'мин', sek_int , 'сек')
+
+#duration = input('duration =')
+#a = float(duration)
+#hour = a // 3600
+#min = (a - hour * 3600) // 60
+#sek = (a - hour * 3600) % 60
+#hour_int = int(hour)
+#min_int = int(min)
+#sek_int = int(sek)
+#print( hour_int, 'час', min_int , 'мин', sek_int , 'сек')
+
+#duration = input('duration =')
+#a = float(duration)
+#day = a // 86400
+#hour = (a - day * 86400) // 3600
+#min = (a - day * 86400 - hour * 3600) // 60
+#sek = (a - day * 86400) % 60
+#day_int = int(day)
+#hour_int = int(hour)
+#min_int = int(min)
+#sek_int = int(sek)
+#print(day_int , 'день', hour_int , 'час', min_int , 'мин', sek_int , 'сек')
+
+
+
+
+#Задание №2
+list = [i for i in range(1, 1001)]
+cube = len(list) ^ 3
+
+# не понимаю(((((((
+
+
+
+
+#Задание 3
+#given = input('Введите число от 1 до 20 - ')
+#numbers = int(given)
+#if numbers == 1:
+#    print(numbers, 'процент')
+#if 1 < numbers < 5:
+#    print(numbers, 'процента')
+#if 4 < numbers < 21:
+#    print(numbers, 'процентов')
+#if numbers > 20:
+#    print('Ошибка! Повторите ввод!')
+
+
+
